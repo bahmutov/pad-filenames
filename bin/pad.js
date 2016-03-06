f3502370e979fe4da9340256e11236dd4192ad79
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-06 15:14:34 -0500

    feat(bin): working on bin script

--- bin/pad.js
+++ bin/pad.js
@@ -1,1 +1,17 @@
+#!/usr/bin/env node
 
+'use strict'
+
+const filenames = process.argv.slcie(2)
+console.log('padding', filenames)
+
+const pad = require('..')
+const padded = pad(filenames)
+filenames.forEach((before, k) => {
+  const after = padded[k]
+  if (before === padded) {
+    console.log('skipping', before)
+    return
+  }
+  console.log('renaming', before, 'to', after)
+})
